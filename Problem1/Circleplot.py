4dea128f77d35f903bf7369e7bff704ff6bac7c6
dhongyeon <dhongyeoncheong@gmail.com>
2017-07-13 00:22:05 +0900

    FINAL

--- Problem1/Circleplot.py
+++ Problem1/Circleplot.py
@@ -2,16 +2,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from math import sin, cos
+
 r = 1
 x = np.linspace(-r,r,1000)
 y = np.sqrt(-x**(2)+r**(2))
 
-theta1 = 2.17
-x1 = r*np.cos(theta1)
-y1 = r*np.sin(theta1)
+theta1 = input("Theta value ")
+theta1 = float(theta1)
+x1 = r*cos(theta1)
+y1 = r*sin(theta1)
 
 b = -(x1/y1)*x+(r/y1)
 
 plt.plot(x,y,'b',x,b,'r')
 plt.plot(x,-y,'b',x,b,'r')
 plt.gca().set_aspect('equal')
+
+plt.show()
--- Problem1/Derivative.py
+++ Problem1/Derivative.py
@@ -1,15 +1,41 @@
-import numpy as np
-import pylab as plt
-import seaborn as sns
-import pandas as pd
+#%%
 
 class Derivative:
-  def __init__(self, f, h=10**(-5)):
-      self.f = f
-      self.h = float(h)
+  def __init__(og, f, h=10**(-5)):
+      og.f = f
+      og.h = float(h)
 
-  def __call__(self, x):
-      return (self.f(x+self.h) - self.f(x))/self.h
+  def __call__(og, x):
+      return (og.f(x+og.h) - og.f(x))/og.h
 
 
 
+from math import sin, cos, exp, log
+
+df = Derivative(sin)
+x = 0
+print("d(sin, 0)=")
+print(df(x))
+
+df = Derivative(cos)
+x = 0
+print("d(cos, 0)=")
+print(df(x))
+
+df = Derivative(exp)
+x = 0
+print("d(exp, 0)=")
+print(df(x))
+
+def y(x):
+    return x**2
+
+df = Derivative(y)
+x = 0
+print("d(x^2, 0)=")
+print(df(x))
+
+df = Derivative(log)
+x = 0
+print("d(ln, 0)=")
+print(df(x))
