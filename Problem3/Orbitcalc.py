1437edb09baddf6d9f2d11dfb5760dd392bad439
dhongyeon <dhongyeoncheong@gmail.com>
2017-07-21 18:06:47 +0900

    Initial

--- Problem3/Orbitcalc.py
+++ Problem3/Orbitcalc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math as mt
 import scipy as sc
 import pandas as pd
 
@@ -7,3 +8,9 @@ m = 5.9736*(10**(24))
 M = 1.9891*(10**(30))
 AU = 1.49597870691*(10*(11))
 
+Radiuss = ((-9.851920196143998*(0.1))**2 + (1.316466809434336*(0.1))**(2)+(4.877392224782687*(10**(-6))**(2)))
+Radius = mt.sqrt(Radiuss)
+
+print(Radius)
+
+print(m)
