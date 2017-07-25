ffd3bf5d31de1fb340a6a67c54564eb5096f7e4f
dhongyeon <dhongyeoncheong@gmail.com>
2017-07-25 13:00:04 +0900

    Final

--- Problem3/Orbitcalc.py
+++ Problem3/Orbitcalc.py
@@ -68,9 +68,9 @@ def Calculation(x):
         a = acceleration(position_vec)
         vf = velocity
         velocity= [(vi + ai*tscale) for vi, ai in zip(velocity, a)]
-        va = [(i+j) for i, j in zip(velocity, vf)]
+        vs = [(i+j) for i, j in zip(velocity, vf)]
         if i == 2:
-            vd = [(i*0.5 - j) for i, j in zip(va, vf)]
+            vd = [(i*0.5 - j) for i, j in zip(vs, vf)]
             
             T.insert(0, Kinetic_Energy([],[(i - j) for i, j in zip(vf, vd)]))
 
@@ -82,8 +82,8 @@ def Calculation(x):
         position_vec = [j+(k*tscale) for j, k in zip(position_vec, velocity)]
         P_vec.insert(i, position_vec)
         
-        T.insert(i-1, Kinetic_Energy(position_vec, [(i*0.5) for i in va]))
-        U.insert(i, Potential_Energy(position_vec, [(i*0.5) for i in va]))
+        T.insert(i-1, Kinetic_Energy(position_vec, [(i*0.5) for i in vs]))
+        U.insert(i, Potential_Energy(position_vec, [(i*0.5) for i in vs]))
 
     return P_vec, T, U
 
